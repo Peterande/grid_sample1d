432f60412410b998469224ddf009a7614d858122
Peterande <52695035+Peterande@users.noreply.github.com>
2023-06-06 11:54:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name='grid_sample1d_cuda',
     ext_modules=[
         CUDAExtension('grid_sample1d_cuda', [
-            'src/grid_sample1d_cuda.cpp',
-            'src/grid_sample1d_cuda_kernel.cu',
+            'grid_sample1d_cuda.cpp',
+            'grid_sample1d_cuda_kernel.cu',
         ])
     ],
     cmdclass={
-        'build_ext': BuildExtension
+        'build_ext': BuildExtension.with_options(use_ninja=False)
     })
